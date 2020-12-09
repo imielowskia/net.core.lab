bc49db3d5ad75ad7d6d238e5642ff0988bbff50a
Andrzej <Andrzej@WIN-DEV>
2020-12-09 14:06:05 +0100

    dodanie obsługi ViewComponent

---
+++ Controllers/GradesController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using CW4.Models;
+
+namespace CW4.Controllers
+{
+    public class GradesController : Controller
+    {
+        private readonly CW4Context _context;
+
+        public GradesController(CW4Context context)
+        {
+            _context = context;
+        }
+
+        // GET: Grades
+        public ActionResult Index()
+        {
+            ViewData["courses"] = _context.CourseGroups.Include(cg => cg.Course).Include(cg => cg.Group).OrderBy(cg=>cg.CourseID).ThenBy(cg=>cg.GroupID);
+            return View();
+        }
+
+        // POST: Save - zapis ocen z viewcomponent GradeList
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Save()
+        {
+            int cid = int.Parse(HttpContext.Request.Form["cid"]);
+            int gid = int.Parse(HttpContext.Request.Form["gid"]);
+            var GList = await _context.Grades.Where(g => g.GroupID == gid).Where(c => c.CourseID == cid).ToListAsync();
+            foreach(var s in GList)
+            {
+                int xgr = int.Parse(HttpContext.Request.Form[s.StudentID.ToString()]);
+                s.Ocena = xgr;
+                s.Data = DateTime.Today;
+                _context.Update(s);
+            }
+            _context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult GradeList(int cr, int gr, int e)
+        {
+            return ViewComponent("GradeList", new { cr, gr, e });
+        }
+    }
+}
---
+++ ViewComponents/GradeListViewComponent.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CW4.Models;
+
+namespace CW4.ViewComponents
+{
+    public class GradeListViewComponent:ViewComponent
+    {
+        private readonly CW4Context _context;
+
+        public GradeListViewComponent(CW4Context context)
+        {
+            _context = context;
+        }
+        public async Task<IViewComponentResult> InvokeAsync(int cr, int gr, int e)
+        {
+            string xview = "Default";
+            ViewData["cid"] = cr;
+            ViewData["gid"] = gr;
+            var items = await _context.Grades.Include(g => g.Student).Where(g => g.CourseID == cr).Where(g => g.GroupID == gr).ToListAsync(); 
+            if (e == 1)
+            {
+                xview = "Edit";
+                var GSList = new List<GS>();
+                foreach (Grade grd in items)
+                {
+                    GSList.Add(new GS
+                    {
+                        StudentID = grd.Student.Id,
+                        ImieNazwisko = grd.Student.ImieNazwisko,
+                        Ocena = grd.Ocena,
+                        Data = grd.Data
+                    });
+                }
+                ViewData["grades"] = GSList;
+            }  
+            if (e == 2)
+            {
+                int cid = int.Parse(HttpContext.Request.Form["cid"]);
+                int gid = int.Parse(HttpContext.Request.Form["gid"]);
+                var GList = items;
+                foreach (var s in GList)
+                {
+                    int xgr = int.Parse(HttpContext.Request.Form[s.StudentID.ToString()]);
+                    s.Ocena = xgr;
+                    s.Data = DateTime.Today;
+                    _context.Update(s);
+                }
+                _context.SaveChanges();
+            }
+            return View(xview ,items);
+        }
+    }
+}
